08b0c8a551337f721b19685cd2bbf754bd752ea3
jonathanreiss15 <jonathanrreiss@gmail.com>
2018-10-03 14:05:03 -0700

    renamed

--- Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-umbrella.h
+++ Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_podtestVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_podtestVersionString[];
+FOUNDATION_EXPORT double Pods_SampleAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SampleAppVersionString[];
 
